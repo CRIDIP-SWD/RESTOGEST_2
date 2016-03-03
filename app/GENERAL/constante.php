abfc9b55ddf051851c18da1eac6cb74cb8d5d332
CRIDIP-SWD <mmockelyn@cridip.com>
2016-03-03 16:24:29 +0100

    RESTOGEST

--- app/GENERAL/constante.php
+++ app/GENERAL/constante.php
@@ -11,11 +11,11 @@ namespace App\GENERAL;
 
 class constante
 {
-    const HTTP              = "http://";
-    const URL               = "gestcom.cridip.com/";
+    const HTTP              = "https://";
+    const URL               = "test.restogest.com/";
     const ASSETS            = "assets/";
-    const NOM_SITE          = "GESTCOM";
-    const SOURCES           = "http://ns342142.ip-5-196-76.eu/sources/gc/";
+    const NOM_SITE          = "RESTOGEST";
+    const SOURCES           = "https://ns342142.ip-5-196-76.eu/sources/gc/";
     const MAINTENANCE       = 0;
     const IP_MAIN           = "109.190.224.161";
     const IP_SRC            = "ns342142.ip-5-196-76.eu";
